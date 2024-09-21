12df5e51c667018d5de935029eccd8ee6ad84c9e
mohit tondre <mohittondre214@gmail.com>
2024-09-21 12:49:30 +0530

    changes

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsmate = require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-// const {listingSchema} = require("./schema.js");
+const { listingSchema, reviewSchema } = require("./schema.js");
+const Review = require("./models/review.js");
 
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/VenueSphere";
@@ -47,6 +48,16 @@ const validateListing =(req, res, next) => {
     }
 };
 
+const validateReview =(req, res, next) => {
+    let (error)= reviewSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    } else{
+        next();  
+    }
+};
+
 //index route
 app.get("/listings", wrapAsync (async (req, res) =>{
     const allListings = await Listing.find ({});
@@ -64,17 +75,17 @@ app.get("/listings/new", (req, res) =>{
 //show route
 app.get("/listings/:id", wrapAsync (async (req, res) => {
    let {id} = req.params;
-   const listing =  await Listing.findById(id);
+   const listing =  await Listing.findById(id).populate("reviews");
    res.render("listings/show.ejs", { listing });
 }));
 
 
 
 //create route
-app.post("/listings",
+app.post("/listings",validateListing,
      wrapAsync (async(req, res, next) => {
     const newListing = new Listing(req.body.listing);
-    newListing.save();
+    await newListing.save();
     res.redirect("/listings");
 }));
 
@@ -107,6 +118,24 @@ app.delete("/listings/:id", wrapAsync (async (req, res) =>{
 }));
 
 
+// review 
+// post route
+
+app.post
+("/listings/:id/reviews",
+    validateReview,
+    wrapAsync(async(req,res) =>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+   
+    await newReview.save();
+    await listing.save();
+
+  res.redirect(`/listings/${listing._id}`);
+}));
+
 
 // app.get("/testListing", async (req, res) =>{
 //     let sampleListing = new Listing({
@@ -125,9 +154,9 @@ app.delete("/listings/:id", wrapAsync (async (req, res) =>{
 // Start the server
 
 
-app.all("*", (req, res, next) =>{
-    next(new ExpressError(404, "page Not Found!"));
-});
+// app.all("*", (req, res, next) =>{
+//     next(new ExpressError(404, "page Not Found!"));
+// });
 
 app.use((err, req, res, next) => {
     let {statusCode = 500, message = "something went wrong"} = err ;
--- schema.js
+++ schema.js
@@ -6,7 +6,14 @@ const listingSchema = Joi.object({
         description: Joi.string().required(),
         location: Joi.string().required(),
         country: Joi.string().required(),
-        price: Joi.number().required(),
+        price: Joi.number().required().min(0),
         Image: Joi.string().allow("", null)
     }).required()
+});
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().required().min(1).max(5),
+        comment: Joi.string().required(),
+    }).required(),
 });
